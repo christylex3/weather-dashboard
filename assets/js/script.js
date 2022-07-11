770e5e79e3c662719896ba0644f7f8f6e78e13d6
Christy Le <christylex3@gmail.com>
2022-07-11 19:58:57 -0400

    Rewriting with jQuery. Adding event listener to search button with jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,74 @@
+// OpenWeather API
+var weatherRequest = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// Geocoding API
+var coordinatesRequest = "http://api.openweathermap.org/geo/1.0/direct?q=";
+
+// API Key for both APIs
 var APIKey = "7d7fd26e34daa0f33590c9e7ba3f4a3f";
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=";
 
-var searchBtn = document.querySelector("#search");
-var city = document.querySelector("#city-input");
+var searchBtn = $("#search");
+var city;
+
+searchBtn.on("click", function() {
+    city = $("#city-input").val();
+})
 
 
-if (searchBtn) {
-    searchBtn.addEventListener("click", getWeather);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var city = document.querySelector("#city-input");
+// console.log(city);
+
+// if (searchBtn) {
+//     searchBtn.addEventListener("click", checksInput);
+// }
+
+// // Checks if input is valid, and if so, call OpenWeather API
+// function checksInput (city) {
+//     if (!isNaN(city)) {
+//         return;
+//     } else {
+        
+//         console.log(city);
+
+        // var cityCoordsRequest = coordinatesRequest + city + "&limit=1&appid=" + APIKey;
+        // getCityCoords(cityCoordsRequest);
+        // var cityWeatherRequest = weatherRequest + city;
+//         // getWeather(cityWeatherRequest);
+//     }
+// }
+
+function getCityCoords(requestURL) {
+    fetch(requestURL).then(function(response) {
+        console.log(response);
+        return response.json;
+    }).then(function(data) {
+        console.log(data);
+    });
 }
 
-// Checks if input is valid, and if so, call OpenWeather API
-function checksInput (city) {
-    if (!isNaN(city)) {
-        return;
-    } else {
-        getWeather(city);
-    }
-}
-
-function getWeather (city) {
-    fetch(queryURL).then
-
-}
+// function getWeather (requestURL) {
+//     fetch(requestURL).then(function(response) {
+//         console.log(response);
+//         return response.json;
+//     }).then(function(data) {
+//         console.log(data); 
+//     });
+// }
