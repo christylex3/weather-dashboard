ab3467eb8645a246ec62929382790eac35cb2c1b
Christy Le <christylex3@gmail.com>
2022-07-11 21:28:38 -0400

    Successfully grabbed longitude and latitude of city from Geocoding API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,19 @@ var weatherRequest = "https://api.openweathermap.org/data/2.5/weather?q=";
 var coordinatesRequest = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
 // API Key for both APIs
-var APIKey = "7d7fd26e34daa0f33590c9e7ba3f4a3f";
+const APIKey = "7d7fd26e34daa0f33590c9e7ba3f4a3f";
 
 var searchBtn = $("#search");
+
 var city;
+var latitude;
+var longitude;
 
 // Grabs the value from the input when search button is clicked
 searchBtn.on("click", function() {
     city = $("#city-input").val();
     checkInput(city);
-})
+});
 
 
 // Checks if the user enters an invalid input
@@ -27,12 +30,33 @@ function checkInput(city) {
 }
 
 function getWeather (city) {
-    console.log("Inside of getWeather");
+    getCoords(city);
+    getCurrentWeather(city);
+}
+
+function getCoords (city) {
+    var cityCoordsRequest = coordinatesRequest + city + "&limit=1&appid=" + APIKey;
+    getLatLong(cityCoordsRequest);
+}
+
+// API call to Geocoding - grabs city's latitude and longitude
+function getLatLong(requestURL) {
+    fetch(requestURL).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        latitude = data[0].lat;
+        longitude = data[0].lon;
+    });
 }
 
 
 
-
+// TODO:
+// 1) Get city's coords
+// 2) Get city's current weather
+// 3) Get city's 5 Day forecast
+// 4) Print out weather info-related appropriately (Temp, Wind, Humidity, UV Index)
+// 5) Set up local storage to store previous searches
 
 
 
@@ -71,14 +95,7 @@ function getWeather (city) {
 //     }
 // }
 
-function getCityCoords(requestURL) {
-    fetch(requestURL).then(function(response) {
-        console.log(response);
-        return response.json;
-    }).then(function(data) {
-        console.log(data);
-    });
-}
+
 
 // function getWeather (requestURL) {
 //     fetch(requestURL).then(function(response) {
